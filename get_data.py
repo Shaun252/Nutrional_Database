a16b2b561312ce7e8e1eff88573da7a7fca50165
Shaun Lahert <shaun.lahert@gmail.com>
2017-03-16 13:34:51 +0000

    Pulled data and turned it into a db

---
+++ get_data.py
@@ -0,0 +1,28 @@
+import requests
+import pandas as pd
+
+food_dict_ndb = {"egg" : "01123", "rice_flour" : "20090", "rice_milk" : "14639", "frozen_raspberries" : "09306", "rice_basmati" : "20044", "chicken" : "05062", "olive_oil" : "04053", "rapeseed_oil" : "04582", "red_pepper" : "11821", "carrot" : "11124", "spinach" : "11457", "cheddar_cheese" : "01009", "rice_cake" : "19053"}
+
+headers = {'Content-Type': 'application/json'}
+df = pd.DataFrame()
+
+for key, val in food_dict_ndb.items():
+	params = {"api_key":"o9Ra8M2RDQKU3XMI4d3XiUzzD8FtwTXeOiH0F2vV", "ndbno":val, "type":"f", "format":"JSON"}
+	response = requests.get('https://api.nal.usda.gov/ndb/reports', headers=headers, params = params)
+
+	data = response.json()
+
+	for j in data["report"]["food"]["nutrients"]:
+		df.loc[key, j["name"] + "_" + j["unit"]] = float(j["value"]) / 100
+
+df = df.fillna(0)
+
+
+df.to_csv('food.csv', encoding='utf-8')
+
+
+
+
+	
+
+
---
+++ process_data.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+data = pd.read_csv("food.csv", index_col=0)
+
+food_intake = {"spinach" : 100, "egg" : 120, "rice_basmatic" : 300, 
+
